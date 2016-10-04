bdce47e66804a03da419288796d33bb0ef143439
a.kelly <a.kelly@accenture.com>
2016-10-04 15:12:57 +0100

    new QR code added

--- public/app.js
+++ public/app.js
@@ -1,73 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import QrReader from 'react-qr-reader'
 import axios from 'axios'
+import login from './Login'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
 var store = {}
-
-class Scan extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      cart: ''
-    };
-  }
-  captureImage(image){
-
-      this.state.context.drawImage(this.state.video, 0, 0, 500, 400);
-
-      qrcode.decode(this.state.canvas.toDataURL());
-      qrcode.callback = (data) => {
-        if(data !== "error decoding QR Code"){
-          var converted = JSON.parse(data);
-          this.setState({ cart: converted })
-          store = converted;
-          this.context.history.replaceState('null', 'checkout')
-        }
-      };
-
-  }
-  componentDidMount(){
-    this.setState({
-      video: ReactDom.findDOMNode(this.refs.video),
-      canvas : ReactDom.findDOMNode(this.refs.canvas),
-      context:  ReactDom.findDOMNode(this.refs.canvas).getContext('2d'),
-    })
-    navigator.getMedia = ( navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia);
-    navigator.getMedia(
-      {
-        video: true,
-        audio: false
-      },
-      (stream) => {
-        let vendorURL = window.URL || window.webkitURL;
-        this.state.video.src = vendorURL.createObjectURL(stream);
-        this.state.video.play();
-      },
-      (err) => {
-        console.log("cart empty");
-      }
-    );
-  }
-  render(){
-    return (
-      <div className="wrapper">
-        <video ref="video" height="100%" width="100%">Video stream not available.</video>
-        <button className="capture-btn btn" onClick={this.captureImage.bind(this)}><i className="fa fa-camera"></i></button>
-        <canvas style={{display:"none"}}  ref="canvas" height="600px" width="600px"></canvas>
-      </div>
-    )
-  }
-}
-
-
-Scan.contextTypes = {
-  cart: React.PropTypes.object,
-  history: React.PropTypes.object
-}
 
 class Container extends React.Component{
   render(){
@@ -79,71 +17,83 @@ class Container extends React.Component{
   }
 }
 
-
 class Checkout extends React.Component{
   render(){
     this.store = store;
     return (
-      <div>
-        {Object.keys(this.store.items).map((i) => {
+      <div>cat
+        {/* {Object.keys(this.store.items).map((i) => {
           return (<li id={i}>{this.store.items[i].name + ' £'+this.store.items[i].price}</li>)
-        })}
+        })} */}
       </div>
     )
   }
 }
 
 Checkout.contextTypes = {
-  cart: React.PropTypes.object,
-  history: React.PropTypes.object
+  result: React.PropTypes.object
 }
 
-class Login extends React.Component{
-  storeToken(data){
-    window.localStorage.setItem('token', data.token);
+class Scan extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      result: '',
+    }
   }
-  login(e){
-    e.preventDefault();
-    axios.post('http://tuckshop.allan.cx/api/v1/authenticate',
-      {
-        'email': ReactDom.findDOMNode(this.refs.email).value,
-        'password': ReactDom.findDOMNode(this.refs.password).value
-      })
-      .then((response) => {
-        this.storeToken(response.data)
-        this.context.history.replaceState(null, '/dashboard')
-      })
-      .catch((response) => console.log('error'+response))
+  handleScan(data){
+    this.setState({
+      result: data,
+    })
+    store = data;
+    console.log(store);
+
+    let alert = confirm('Ordered Successfully');
+    if(alert == true) {
+      browserHistory.push('/checkout')
+    }
+
+    // axios.post('', data)
+    //   .then(function(response){
+    //     console.log('Ordered Successfully')
+    //     browserHistory.push('checkout');
+    //   }).catch((response) => console.log('error'+response));
+
+  }
+  handleError(err){
+    console.error(err)
   }
   render(){
-    return (
-      <div className="flex-center-align">
-        <section className="panel">
-          <h2>Sign In</h2>
-          <form ref="form" onSubmit={this.login.bind(this)}>
-            <label >Email</label>
-            <input name="email" ref="email" type="text" /><br/>
+    const previewStyle = {
+      height: 600,
+      width: 600,
+    }
 
-            <label >Password</label>
-            <input name="password" ref="password" type="password" /><br/>
-            <input value="Sign In" type="submit" />
-          </form>
-        </section>
+    return(
+      <div className="wrapper">
+        <QrReader
+          className="QR"
+          previewStyle={previewStyle}
+          handleError={this.handleError}
+          handleScan={this.handleScan.bind(this)}/>
+        <p>{this.state.result}</p>
       </div>
     )
   }
 }
 
-Login.contextTypes = {
+Scan.contextTypes = {
+  result: React.PropTypes.object,
   history: React.PropTypes.object
+
 }
 
 class Header extends React.Component{
   render(){
     return (
       <header className="bar-header">
-        <div className="button-box"></div>
-        <h2>IFM </h2>
+      <div className="button-box"></div>
+        <h2>Tuck Shop </h2>
         <div className="button-box">
           <Link className="btn" to="/scan">Scan</Link>
         </div>
@@ -175,7 +125,6 @@ class Dashboard extends React.Component{
       .catch((response) => console.log('error'+response))
   }
   render(){
-    console.log('hello')
     return (
       <div className="container">
         <Header />
@@ -201,19 +150,58 @@ class Dashboard extends React.Component{
   }
 }
 
-var redirect = (nextState, replaceState) => {
+class Login extends React.Component{
+    storeToken(data){
+    window.localStorage.setItem('token', data.token);
+  }
+  login(e){
+    e.preventDefault();
+    axios.post('http://tuckshop.allan.cx/api/v1/authenticate',
+      {
+        'email': ReactDom.findDOMNode(this.refs.email).value,
+        'password': ReactDom.findDOMNode(this.refs.password).value
+      })
+      .then((response) => {
+        this.storeToken(response.data)
+        this.context.history.replaceState(null, '/dashboard')
+      })
+      .catch((response) => console.log('error'+response))
+  }
+  render(){
+    return (
+      <div className="flex-center-align login">
+        <section>
+          <h2>Sign In</h2>
+          <form ref="form" onSubmit={this.login.bind(this)}>
+            <input name="email" ref="email" placeholder="Email" type="text" /><br/>
+
+            <input name="password" ref="password" placeholder="Password" type="password" /><br/>
+            <input value="Sign In" type="submit" />
+          </form>
+        </section>
+      </div>
+    )
+  }
+}
+
+Login.contextTypes = {
+  history: React.PropTypes.object
+}
+
+
+var redirect = (nextState, replace) => {
   let token = window.localStorage.getItem('token');
   if(token !== null){
-    replaceState(null, '/dashboard');
+    replace('/dashboard');
   }else{
     return true
   }
 }
 
-var auth = (nextState, replaceState) => {
+var auth = (nextState, replace) => {
   let token = window.localStorage.getItem('token');
   if(token === null){
-    replaceState(null, '/');
+    replace('/');
   }else{
     return true
   }
@@ -226,10 +214,10 @@ let routes = {
   indexRoute: { component: Login, onEnter:redirect  },
   childRoutes: [
     { path: 'dashboard', component: Dashboard, onEnter: auth},
-    { path: 'scan', component: Scan, onEnter: auth},
     { path: 'checkout', component: Checkout, onEnter: auth},
+    { path: 'scan', component: Scan, onEnter: auth}
   ]
 };
 
 
-ReactDom.render(<Router routes={routes} />, document.getElementById('react-app'));
+ReactDom.render(<Router routes={routes} history={browserHistory}/>, document.getElementById('react-app'));
